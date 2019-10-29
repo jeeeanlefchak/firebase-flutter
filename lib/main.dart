02f37db193127f752793eda0492f74ea1720097a
jeeeanlefchak <jean_2103@hotmail.com>
2019-10-29 20:58:35 -0300

    k

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
